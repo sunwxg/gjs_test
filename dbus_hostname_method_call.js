ec8fce422ab2c53c8eb7fb91dcfc5f51708f18c7
sunwxg <sun.wxg@gmail.com>
2017-10-12 15:27:54 +0800

    updating

--- dbus_hostname_method_call.js
+++ dbus_hostname_method_call.js
@@ -2,26 +2,31 @@ const Gio = imports.gi.Gio;
 
 const HostnameIface = '<node> \
 <interface name="org.freedesktop.DBus.Properties"> \
-<method name="Get"> \
-    <arg type="s" direction="in" /> \
-    <arg type="s" direction="in" /> \
-    <arg type="v" direction="out" /> \
-</method> \
+    <method name="Get"> \
+        <arg type="s" direction="in" /> \
+        <arg type="s" direction="in" /> \
+        <arg type="v" direction="out" /> \
+    </method> \
 </interface> \
 </node>';
 
-let HostnameProxy = Gio.DBusProxy.makeProxyWrapper(HostnameIface);
+const HostnameProxy = Gio.DBusProxy.makeProxyWrapper(HostnameIface);
 
-function Hostname(initCallback, cancellable) {
-    return new HostnameProxy(Gio.DBus.system, 'org.freedesktop.hostname1',
-            '/org/freedesktop/hostname1', initCallback, cancellable);
-}
+//function Hostname(initCallback, cancellable) {
+    //return new HostnameProxy(Gio.DBus.system,
+                             //'org.freedesktop.hostname1',
+                             //'/org/freedesktop/hostname1',
+                             //initCallback, cancellable);
+//}
 
 
-let hostname = new Hostname();
+let hostname = HostnameProxy(Gio.DBus.system,
+                             'org.freedesktop.hostname1',
+                             '/org/freedesktop/hostname1',
+                             null, null);
 
 let name = hostname.GetSync('org.freedesktop.hostname1', 'Hostname');
 print(name[0].get_string()[0]);
 
-let name = hostname.GetSync('org.freedesktop.hostname1', 'StaticHostname');
+name = hostname.GetSync('org.freedesktop.hostname1', 'StaticHostname');
 print(name[0].get_string()[0]);
