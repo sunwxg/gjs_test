128a59e12fc2ee295fa4c1b36bf8db83f16a9383
sunwxg <sun.wxg@gmail.com>
2017-11-23 15:38:56 +0800

    updating

--- gjs_clutter.js
+++ gjs_clutter.js
@@ -1,4 +1,4 @@
-const Cogl = imports.gi.Cogl = 3.0;
+const Cogl = imports.gi.Cogl = 1.0;
 const Clutter = imports.gi.Clutter;
 
 const Gio = imports.gi.Gio;
--- prototype-runtime.js
+++ prototype-runtime.js
@@ -1,17 +1,18 @@
 #!/usr/bin/env gjs-console
 
 var Book = function(isbn, title) {
-	this.isbn = isbn;
-	this.title = title;
-}
+    this.isbn = isbn;
+    this.title = title;
+};
+
 Book.prototype = {
-	printTitle: function(){
-		print("Title is " + this.title);
-	},
-	printISBN: function() {
-		print("ISBN is " + this.isbn);
-	}
-}
+    printTitle: function(){
+        print("Title is " + this.title);
+    },
+    printISBN: function() {
+        print("ISBN is " + this.isbn);
+    }
+};
 var book = new Book("1234", "A good title");
 
 book.printTitle();
@@ -19,11 +20,11 @@ book.printISBN();
 
 //runtime prototype modifcation.
 book.__proto__ = {
-	author: "Joe Random",
-	printAuthor: function() {
-		print("Author is " + this.author);
-	}
-}
+    author: "Joe Random",
+    printAuthor: function() {
+        print("Author is " + this.author);
+    }
+};
 
 book.printAuthor();
 
@@ -32,3 +33,15 @@ var anotherBook = new Book("4567", "A more better title");
 anotherBook.printTitle();
 anotherBook.printISBN();
 //anotherBook.printAuthor(); // this is invalid
+
+//new prototype
+Book.prototype = {
+    printTitle: function(){
+        print("New Title is " + this.title);
+    }
+};
+
+anotherBook.printTitle();
+
+let newBoot = new Book("xxxx", "A New proto title");
+newBoot.printTitle();
